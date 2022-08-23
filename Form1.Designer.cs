5af6809fbc55ed4a672708562f0c8c37e993c511
Likhaya A. Faku <arnoldfaku@gmail.com>
2022-08-23 21:52:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace mileage_efficiency
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.txtGallons = new System.Windows.Forms.TextBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(105, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mileage Efficiency Calclutor";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Miles Driven:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gallons of gas used: ";
+            this.label3.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Efficiency (Miles/Gallon):";
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(430, 126);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(150, 31);
+            this.txtMiles.TabIndex = 4;
+            // 
+            // txtGallons
+            // 
+            this.txtGallons.Location = new System.Drawing.Point(430, 180);
+            this.txtGallons.Name = "txtGallons";
+            this.txtGallons.Size = new System.Drawing.Size(150, 31);
+            this.txtGallons.TabIndex = 5;
+            // 
+            // txtResults
+            // 
+            this.txtResults.AcceptsReturn = true;
+            this.txtResults.AcceptsTab = true;
+            this.txtResults.AllowDrop = true;
+            this.txtResults.Location = new System.Drawing.Point(430, 238);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.Size = new System.Drawing.Size(150, 31);
+            this.txtResults.TabIndex = 6;
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.AllowDrop = true;
+            this.BtnCalculate.Location = new System.Drawing.Point(449, 332);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(112, 34);
+            this.BtnCalculate.TabIndex = 7;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipTitle = "Please enter a value > 0 ";
+            this.toolTip1.UseAnimation = false;
+            this.toolTip1.UseFading = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.RightToLeft = true;
+            this.errorProvider1.Tag = "Please enter value > 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtGallons);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.TextBox txtGallons;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button BtnCalculate;
+        public System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mileage_efficiency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void errorMessagePopUp()
+        {
+            int miles;
+            int gallons;
+            int results;
+
+            string filename = @"C:\Users\arnol\Downloads\MilesperGallon.txt";
+            string filename2 = @"C:\Users\arnol\Exceptionfile.txt";
+            try
+            {
+                miles = int.Parse(txtMiles.Text);
+                gallons = int.Parse(txtGallons.Text);
+                results = (int)miles / (int)gallons;
+                txtResults.Text = results.ToString();
+               
+
+                if (File.Exists(filename))
+                {
+                    StreamWriter writer = File.AppendText(filename);
+                    writer.WriteLine("Miles Drive: " + miles + " Gallons used: " + gallons + " Miles per gallon: " + results);
+                    writer.Close();
+                }
+                else
+                {
+                    StreamWriter writer = File.CreateText(filename);
+                    writer.WriteLine("Miles Drive: " + miles + " Gallons used: " + gallons + " Miles per gallon: " + results);
+                    writer.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                errorProvider1.SetError(txtGallons, e.Message);
+
+                if (File.Exists(filename2))
+                {
+                    StreamWriter writer = File.AppendText(filename2);
+                    writer.WriteLine("Error");
+                    writer.Close();
+                }
+                else
+                {
+                    StreamWriter writer = File.CreateText(filename2);
+                    writer.WriteLine("Exception file");
+                    writer.Close();
+                }
+            }
+            finally
+            {
+                MessageBox.Show("bye");
+            }
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            errorMessagePopUp();
+        }
+    }
+}
